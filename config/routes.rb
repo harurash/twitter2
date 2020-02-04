1b1d211408def8de0ca75e32f6238595974dfd7d
harurash <dgkteriyama@gmail.com>
2020-02-04 16:22:27 +0900

    conect

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "tweets#index"
+  devise_for :users
+  resources :tweets do
+    resources :favorites,only:[:create,:destroy]
+  end
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
